4498e2be2471df4f6900d03cb027cf040ba1ac01
Safturento <safturento@gmail.com>
2017-04-03 16:43:09 -0500

    Added unit testing for paletteManager and modifed updating to a more secure method using componentWillMount

--- web-app/src/component/paletteManager.js
+++ web-app/src/component/paletteManager.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var Swatch = require('./swatch')
 
+const DEBUG = true;
+
 class PaletteManager extends React.Component {
 	constructor(props){
 		super(props)
@@ -9,24 +11,37 @@ class PaletteManager extends React.Component {
 		//The state should never be directly modified, so we update our values in 
 		// this.palette and then update the state when needed
 		this.palette = {}
-		this.setState({ palette:this.palette })
+		this.state={ palette:this.palette }
 
 		// generate random test colors
-		while (this.nextID < Math.floor(Math.random()*3+4))
-			this.addSwatch('#' + (Math.random()*0xFFFFFF<<0).toString(16))
+		if (DEBUG)
+			while (this.nextID < Math.floor(Math.random()*3+4))
+				this.addSwatch('#' + (Math.random()*0xFFFFFF<<0).toString(16))
+	}
+
+	getInitialState(){
+		return { palette:this.palette }
 	}
 
 	addSwatch(color){
-		this.nextID++
+		if(!/^#[0-9a-f]{3,6}$/i.test(color))
+      throw('Invalid color for addSwatch');
 
-		this.palette[this.nextID] = color
-		this.setState({ palette: this.palette })
-
-		console.log(this.getPaletteInfo());
+		this.palette[this.nextID++] = color
 	}
 
 	removeSwatch(key){
+		console.log(this.palette);
+		console.log(key);
+		if(this.palette[key] == undefined)
+      throw('Key doesn\'t exist in palette');
+
 		delete this.palette[key];
+		console.log(this.palette);
+		this.setState({ palette:this.palette })
+	}
+
+	componentWillMount(){
 		this.setState({ palette:this.palette })
 	}
 
@@ -37,16 +52,31 @@ class PaletteManager extends React.Component {
 
 	}
 
-	getPaletteInfo(){
+	getPaletteLength(){
+		return (this.state.palette).length()
+	}
+
+	getPaletteAsString(){
 		return Object.values(this.palette).join(', ')
 	}
 
+	getPaletteInfo(){
+		return this.palette
+	}
+
 	render(){
+		//Keep it populated for testing
+		if (DEBUG)
+			if (Object.keys(this.palette).length < 2)
+				while (Object.keys(this.palette).length < 6)
+					this.addSwatch('#' + (Math.random()*0xFFFFFF<<0).toString(16))
+		
 		return(
 			<div id="palette">
 				{
 					Object.keys(this.palette).map(function(key){
-						return <Swatch className="swatch" deleteFunc={this.removeSwatch.bind(this, key)} id={key} color={this.palette[key]} />
+						console.log(key);
+						return <Swatch className="swatch" deleteFunc={this.removeSwatch.bind(this, key)} id={key} key={key} color={this.palette[key]} />
 					}.bind(this))
 				}
 			</div>
